98f27d18b3ccdf2b1192d59557eb4cd04ddad66a
Nguyen Thanh Nhut <>
2020-06-06 12:09:10 +0700

    config deloy

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     gooleClientID: '287818477541-q905d77othv431di8tqv6a1hfkljl4aa.apps.googleusercontent.com',
     googleClientSecret: 'dAAWBwIp6rW2Y74qCVdlqJ64',
-    mongoURI: `mongodb+srv://db_auth_google:sang517119@cluster0-vob0i.mongodb.net/db_test?retryWrites=true&w=majority`,
+    mongoURI: process.env.MONGOLAB_URI,//`mongodb+srv://db_auth_google:sang517119@cluster0-vob0i.mongodb.net/db_test?retryWrites=true&w=majority`,
     cookieKey:'asdasdqwewqxxzcvcvfgadgaafds'
 };
