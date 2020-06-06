e43c83caaac94651d7b6fa4560f83c9c4c535721
Nguyen Thanh Nhut <>
2020-06-06 12:22:34 +0700

    update key cònig

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     gooleClientID: '287818477541-q905d77othv431di8tqv6a1hfkljl4aa.apps.googleusercontent.com',
     googleClientSecret: 'dAAWBwIp6rW2Y74qCVdlqJ64',
-    mongoURI: process.env.MONGOLAB_URI || `mongodb+srv://db_auth_google:sang517119@cluster0-vob0i.mongodb.net/db_test?retryWrites=true&w=majority`,
+    mongoURI: process.env.MONGOLAB_URI || "mongodb+srv://admin:datn.qlsk.2020@cluster0-9m5xy.mongodb.net/test?retryWrites=true&w=majority",
     cookieKey:'asdasdqwewqxxzcvcvfgadgaafds'
 };
