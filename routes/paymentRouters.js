69868b51167ad3a4ffbfb99346e6258c81c8e0ad
Sang Nguyen <vansangnguyen1998@gmail.com>
2020-06-09 11:08:03 +0700

    payment api

--- routes/paymentRouters.js
+++ routes/paymentRouters.js
@@ -14,7 +14,8 @@ router.post('/add_card', Unauthorized, payment_Controller.create_customer);
 // router.post('/zalopay_create_order', payment_Controller.create_order);
 router.post('/create_order_callback', Unauthorized, payment_Controller.create_order_callback);
 
-router.get('/paymentHistorySend', Unauthorized, payment_Controller.paymentHistorySend);
-router.get('/paymentHistoryReceive', Unauthorized, payment_Controller.paymentHistoryReceive);
+router.get('/payment_history', Unauthorized, payment_Controller.paymentHis);
+
+
 
 module.exports = router;
