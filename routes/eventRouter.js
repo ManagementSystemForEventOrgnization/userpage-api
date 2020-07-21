21c640864014b1500b5e49c099e233a1f67cc7e3
Nguyen Thanh Nhut <nhutnt@hybrid-technologies.vn>
2020-07-21 12:59:11 +0700

    update author for api

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -12,12 +12,12 @@ router.post('/save/page_event', Unauthorized, eventController.savePageEvent);
 router.get('/event', eventController.getPageEvent);
 router.get('/get_list_event', eventController.getListEvent);
 router.get('/get_list_event_coming_up', eventController.getListEventComingUp);
-router.get('/get_event_inf', eventController.getEventInf);
-router.get('/get_user_join_event', eventController.getUserJoinEvent);
+router.get('/get_event_inf', Unauthorized, eventController.getEventInf);
+router.get('/get_user_join_event', Unauthorized, eventController.getUserJoinEvent);
 router.get('/test',eventController.test);
 router.post('/require_edit_event', Unauthorized, eventController.require_edit_event);
 
-router.post('/public_private_event', eventController.publicPrivateEvent);
-router.post('/publish_event', eventController.publishEvent);
+router.post('/public_private_event', Unauthorized, eventController.publicPrivateEvent);
+router.post('/publish_event', Unauthorized, eventController.publishEvent);
 
 module.exports = router;
